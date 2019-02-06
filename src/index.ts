9f2dd6463e10f8aae018813f89fb178b1f782a7c
binau <aubin.roumillac@gmail.com>
2019-02-06 16:43:35 +0100

    Modification client ws

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export {HttpClient} from './client/http.client';
 export {WsServerInterface} from './server/ws.server.interface';
 export {WsServer} from './server/ws.server';
 export {WsServerParam} from './server/ws.server.param';
+
+export {ClientNodeWs} from './client/client.node.ws';
